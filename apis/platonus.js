02eb748ed16b22a87bd9d7291b046872bc75b882
Devil Devilson <50243012+devildevilson@users.noreply.github.com>
2023-09-06 14:22:08 +0600

    added data for en certs

--- apis/platonus.js
+++ apis/platonus.js
@@ -37,32 +37,36 @@ const db = {
   find_student_data_for_certificate: async (student_id) => {
     const query_str = `
       SELECT 
-        s.firstname AS name,
+        s.firstname,
         s.lastname,
-        s.patronymic AS middlename,
+        s.patronymic,
         s.iinplt AS iin,
         s.StartDate AS start_date,
         s.BirthDate AS birth_date,
         s.StudyFormID AS study_form_id,
-        sf.NameRu AS study_form_name_ru,
-        sf.NameKz AS study_form_name_kz,
         spec.specializationCode AS specialization_code,
-        spec.nameru AS specialization_name_ru,
-        spec.namekz AS specialization_name_kz,
         s.grant_type AS grant_type,
         sf.courseCount AS course_count,
         s.CourseNumber AS course_number,
+        sf.NameRu AS study_form_name_ru,
+        sf.NameKz AS study_form_name_kz,
+        sf.NameEn AS study_form_name_en,
+        spec.nameru AS specialization_name_ru,
+        spec.namekz AS specialization_name_kz,
+        spec.nameen AS specialization_name_en,
         c.cafedraNameRU AS cafedra_ru,
         f.facultyNameRU AS dekanat_ru,
         c.cafedraNameKZ AS cafedra_kz,
-        f.facultyNameKZ AS dekanat_kz
+        f.facultyNameKZ AS dekanat_kz,
+        c.cafedraNameEN AS cafedra_en,
+        f.facultyNameEN AS dekanat_en
       FROM students s
       JOIN studyforms sf ON sf.Id = s.StudyFormID
       JOIN specializations spec ON spec.id = s.specializationID
       JOIN profession_cafedra pc ON spec.prof_caf_id = pc.id
       JOIN cafedras c ON pc.cafedraID = c.cafedraID
       JOIN faculties f ON c.FacultyID = f.FacultyID
-      WHERE s.StudentID = ${student_id} AND s.isStudent = 1;
+      WHERE StudentID = ${student_id} AND s.isStudent = 1;
     `;
 
     const [ res ] = await query_f(query_str);
