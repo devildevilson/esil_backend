9349ee26b9f306b44a43637cbc2d776137ebcc1e
zhan-star <janik150802@gmail.com>
2024-07-04 16:17:31 +0500

    stats test

--- routes/applicant/statsMain.js
+++ routes/applicant/statsMain.js
@@ -14,6 +14,89 @@ module.exports = [
         method: 'GET',
         handler: async function (request, reply) {
             let stats = await plt.form_admission_stats_main();
+            let sf1_sl1count = 0;
+            let sf1_sl2count = 0;
+            let sf3_sl1count = 0;
+            let sf3_sl2count = 0;
+            let sf4_sl1count = 0;
+            let sf4_sl2count = 0;
+            let sf5_sl1count = 0;
+            let sf5_sl2count = 0;
+            let sf8_sl1count = 0;
+            let sf8_sl2count = 0;
+            let sf15_sl1count = 0;
+            let sf15_sl2count = 0;
+            let sf17_sl1count = 0;
+            let sf17_sl2count = 0;
+            let sf21_sl1count = 0;
+            let sf21_sl2count = 0;
+            let sf23_sl1count = 0;
+            let sf23_sl2count = 0;
+            let sf24_sl1count = 0;
+            let sf24_sl2count = 0;
+            let sf29_sl1count = 0;
+            let sf29_sl2count = 0;
+            let sf30_sl1count = 0;
+            let sf30_sl2count = 0;
+            let sf31_sl1count = 0;
+            let sf31_sl2count = 0;
+            for (res of stats) {
+                sf1_sl1count += parseInt(res.sf1_sl1);
+                sf1_sl2count += parseInt(res.sf1_sl2);
+                sf3_sl1count += parseInt(res.sf3_sl1);
+                sf3_sl2count += parseInt(res.sf3_sl2);
+                sf4_sl1count += parseInt(res.sf4_sl1);
+                sf4_sl2count += parseInt(res.sf4_sl2);
+                sf5_sl1count += parseInt(res.sf5_sl1);
+                sf5_sl2count += parseInt(res.sf5_sl2);
+                sf8_sl1count += parseInt(res.sf8_sl1);
+                sf8_sl2count += parseInt(res.sf8_sl2);
+                sf15_sl1count += parseInt(res.sf15_sl1);
+                sf15_sl2count += parseInt(res.sf15_sl2);
+                sf17_sl1count += parseInt(res.sf17_sl1);
+                sf17_sl2count += parseInt(res.sf17_sl2);
+                sf21_sl1count += parseInt(res.sf21_sl1);
+                sf21_sl2count += parseInt(res.sf21_sl2);
+                sf23_sl1count += parseInt(res.sf23_sl1);
+                sf23_sl2count += parseInt(res.sf23_sl2);
+                sf24_sl1count += parseInt(res.sf24_sl1);
+                sf24_sl2count += parseInt(res.sf24_sl2);
+                sf29_sl1count += parseInt(res.sf29_sl1);
+                sf29_sl2count += parseInt(res.sf29_sl2);
+                sf30_sl1count += parseInt(res.sf30_sl1);
+                sf30_sl2count += parseInt(res.sf30_sl2);
+                sf31_sl1count += parseInt(res.sf31_sl1);
+                sf31_sl2count += parseInt(res.sf31_sl2);
+            }
+            stats.push({
+                specialization: 'Всего',
+                sf1_sl1: sf1_sl1count + '',
+                sf1_sl2: sf1_sl2count + '',
+                sf3_sl1: sf3_sl1count + '',
+                sf3_sl2: sf3_sl2count + '',
+                sf4_sl1: sf4_sl1count + '',
+                sf4_sl2: sf4_sl2count + '',
+                sf5_sl1: sf5_sl1count + '',
+                sf5_sl2: sf5_sl2count + '',
+                sf8_sl1: sf8_sl1count + '',
+                sf8_sl2: sf8_sl2count + '',
+                sf15_sl1: sf15_sl1count + '',
+                sf15_sl2: sf15_sl2count + '',
+                sf17_sl1: sf17_sl1count + '',
+                sf17_sl2: sf17_sl2count + '',
+                sf21_sl1: sf21_sl1count + '',
+                sf21_sl2: sf21_sl2count + '',
+                sf23_sl1: sf23_sl1count + '',
+                sf23_sl2: sf23_sl2count + '',
+                sf24_sl1: sf24_sl1count + '',
+                sf24_sl2: sf24_sl2count + '',
+                sf29_sl1: sf29_sl1count + '',
+                sf29_sl2: sf29_sl2count + '',
+                sf30_sl1: sf30_sl1count + '',
+                sf30_sl2: sf30_sl2count + '',
+                sf31_sl1: sf31_sl1count + '',
+                sf31_sl2: sf31_sl2count + ''
+            });
             return stats;
         },
     },
